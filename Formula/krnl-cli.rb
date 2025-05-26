0e9a6e37bd346ea21c9d492ba43cbfcf04a1f34c
Aswin Kumar <86375577+Ash20pk@users.noreply.github.com>
2025-05-26 13:33:50 +0530

    Update krnl-cli.rb

--- Formula/krnl-cli.rb
+++ Formula/krnl-cli.rb
@@ -12,26 +12,22 @@ class KrnlCli < Formula
   end
 
   depends_on "node@18"
-  
+
   def install
-    # Install dependencies
-    system "npm", "install"
-    
-    # Build the project
-    system "npm", "run", "build"
-    
+    # Skip npm install and build steps - use pre-built package
+
     # Install to libexec
     libexec.install Dir["*"]
-    
+
     # Create bin stubs
     (bin/"krnl").write <<~EOS
       #!/bin/bash
       exec "#{Formula["node@18"].opt_bin}/node" "#{libexec}/bin/cli.js" "$@"
     EOS
-    
+
     # Make bin stub executable
     chmod 0755, bin/"krnl"
-    
+
     # Note about Foundry requirement
     ohai "KRNL CLI works best with Foundry installed"
     opoo "To install Foundry, run: curl -L https://foundry.paradigm.xyz | bash"
